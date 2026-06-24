319cd14a604db1bb5f387536d1caff2daadb90ad
Nayem khan <nayem178548@gmail.com>
2026-06-24 06:01:49 +0100

    syncing

---
+++ app/(root)/user/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { auth } from '@/auth';
+import { AUTHOR_BY_ID_QUERY } from '@/sanity/lib/queries';
+import {client} from '@/sanity/lib/client'
+import { notFound } from 'next/navigation';
+import React from 'react'
+
+const Page =async ({params}:{params:Promise<{id:string}>}) => {
+  
+  const id=(await params).id;
+
+const session=await auth();
+
+const user=await client.fetch(AUTHOR_BY_ID_QUERY,{id});
+
+if(!user) return notFound();
+
+    return (
+    <div>Page</div>
+  )
+}
+
+export default Page
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -39,3 +39,8 @@ export const AUTHOR_BY_GITHUB_ID_QUERY=defineQuery(`*[_type=="author" && id==$id
 _id, id,name,username,email,image,bio
 
   }`);
+
+  export const AUTHOR_BY_ID_QUERY=defineQuery(`*[_type=="author" && _id==$id][0]{
+_id, id,name,username,email,image,bio
+
+  }`);
