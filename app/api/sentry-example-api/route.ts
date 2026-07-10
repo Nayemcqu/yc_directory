e9bf16578ac0c76e4998709c9e30ec9544583f27
Nayem Khan <nayem.khan@cqumail.com>
2026-07-10 18:42:35 +1000

    sentry error example page removed

--- app/api/sentry-example-api/route.ts
+++ app/api/sentry-example-api/route.ts
@@ -1,15 +1,10 @@
+import { NextResponse } from "next/server";
 import * as Sentry from "@sentry/nextjs";
-class SentryExampleAPIError extends Error {
-  constructor(message: string | undefined) {
-    super(message);
-    this.name = "SentryExampleAPIError";
-  }
-}
 
-// A faulty API route to test Sentry's error monitoring
 export function GET() {
   Sentry.logger.info("Sentry example API called");
-  throw new SentryExampleAPIError(
-    "This error is raised on the backend called by the example page.",
-  );
+
+  return NextResponse.json({
+    message: "Sentry API is working.",
+  });
 }
